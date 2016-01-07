119481f200719a374a20183e92e27ed854f6bf31
justinav <justinavil@gmail.com>
2016-01-06 23:06:22 -0500

    removing duplicate files

--- x_old/anagram.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'reverse_letters'
-
-def find_anagram(word)
-	letters = word.split("")
-	length = letters.count
-	new_words = letters.permutation(length).map(&:join)
-	return new_words
-end
